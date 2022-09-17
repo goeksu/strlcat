4d832ba26fe4b84093c619373541310a1a274973
goksu <25721443+goeksu@users.noreply.github.com>
2022-09-18 00:44:33 +0300

    Create strlcat.c

---
+++ strlcat.c
@@ -0,0 +1,38 @@
+unsigned int	str_length(char *dest)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (dest[count] != '\0')
+		count++;
+	return (count);
+}
+
+unsigned int	strlcat(char *dest, char *src, unsigned int size)
+{
+	char			*dst;
+	char			*src_start;
+	unsigned int	dst_length;
+	unsigned int	remaing;
+
+	dst = dest;
+	src_start = src;
+	remaing = size;
+	while (remaing-- != 0 && *dst != '\0')
+		dst++;
+	dst_length = dst - dest;
+	remaing = size - dst_length;
+	if (remaing == 0)
+		return (dst_length + str_length(src));
+	while (*src != '\0')
+	{
+		if (remaing > 1)
+		{
+			*dst++ = *src;
+			remaing--;
+		}
+		src++;
+	}
+	*dst = '\0';
+	return (dst_length + (src - src_start));
+}
